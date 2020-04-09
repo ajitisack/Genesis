69fa1bb015ca61c495374f2729ee8e4845760b82
unknown <ajitisack@gmail.com>
2020-04-09 16:30:29 +0530

    updated to NSE precede BSE

--- downloaddata.py
+++ downloaddata.py
@@ -2,5 +2,6 @@ import stockdata as sd
 
 sd.download(startdt='2010-01-01')
 
-sql = 'select * from security where 1 = 1 and inbse = 1'
+sql = 'select * from quotes where 1 = 1 limit 10'
 df = sd.getdata(sql)
+df
