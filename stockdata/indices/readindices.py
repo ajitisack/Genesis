049459a37af63143fb80de070cadacc918342275
unknown <ajitisack@gmail.com>
2020-07-29 23:00:23 +0530

    Modified indices package to remove py files for downloading indices hist prices

--- stockdata/indices/readindices.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-class IndicesList():
-
-    def readIndices(self, exchange):
-        df = pd.read_excel(self.indices_file, sheet_name=exchange.upper())
-        df = df[df.url.notna()]
-        return df
