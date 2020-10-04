c2b9b227d50ea6bd559d2cbc1060325a9c3d1cb0
Ajit Isack <ajit@Ajits-MacBook-Pro.local>
2020-10-04 11:31:55 +0530

    updated dashboard to separate out each sector into different tables

--- dash/app.py
+++ dash/app.py
@@ -196,21 +196,29 @@ def getpricelevel(x):
     if x['open'] == x['high']: pricelevel += ' (OH)'
     return pricelevel
 
-def getprice(rpt=80):
+def getprice(rpt=80, riskpct=0.3):
+    risk = riskpct/100
     df = sd.getdata("select * from  NSE_MyWatchlistCurrentPrice")
+    # df = sd.getdata("select sector, symbol, open, 0 low, 0 high, open ltp, volume, changepct from preopen where inhotlist = 1")
     df = pd.merge(df, pivots, how = 'outer', on = 'symbol')
     df['pricelevel'] = df.apply(lambda x: getpricelevel(x), axis = 1)
     df['volume']     = df['volume'].apply(lambda x: round(x/100000, 2))
-    df['qty']        = df.apply(lambda x: round(rpt/(x['ltp']*0.005), 0), axis = 1)
-    df['slb']        = df['ltp'].apply(lambda x: round(x-(x*0.005),0))
-    df['sls']        = df['ltp'].apply(lambda x: round(x+(x*0.005),0))
-    cols = ['sector', 'symbol', 'ltp', 'volume', 'changepct', 'qty', 'slb', 'sls', 'pricelevel']
-    df_symbols = df[df.sector != 'Index'][cols]
+    df['qty']        = df.apply(lambda x: round(rpt/(x['ltp']*risk), 0), axis = 1)
+    df['slb']        = df['ltp'].apply(lambda x: round(x-(x*risk),0))
+    df['sls']        = df['ltp'].apply(lambda x: round(x+(x*risk),0))
     cols = ['symbol', 'ltp', 'changepct']
     df_indices1 = df[df.symbol == 'Nifty 50'][cols]
     df_indices2 = df[(df.sector == 'Index') & (df.symbol != 'Nifty 50')][cols]
     df_indices2 = df_indices2.sort_values(by='changepct', ascending=False)
-    return df_symbols, df_indices1, df_indices2
+    cols = ['sector', 'symbol', 'ltp', 'volume', 'changepct', 'qty', 'slb', 'sls', 'pricelevel']
+    df_symbols = df[df.sector != 'Index'][cols]
+    df_banks   = df[df.sector == 'Bank'][cols].sort_values(by='changepct', ascending=False)
+    df_it      = df[df.sector == 'IT'][cols].sort_values(by='changepct', ascending=False)
+    df_auto    = df[df.sector == 'Auto'][cols].sort_values(by='changepct', ascending=False)
+    df_pharma  = df[df.sector == 'Pharma'][cols].sort_values(by='changepct', ascending=False)
+    df_metal   = df[df.sector == 'Metal'][cols].sort_values(by='changepct', ascending=False)
+    return df_symbols, df_indices1, df_indices2, df_banks, df_it, df_auto, df_pharma, df_metal
+
 
 def layout():
     body = dbc.Container([
@@ -219,12 +227,38 @@ def layout():
             dbc.Col(html.Div(pricetbl('tbl4', getprice()[1]), id='stock-data4'), width=0)
           ])
         , dbc.Row([
-            dbc.Col(html.Div(pricetbl('tbl1', getprice()[0]), id='stock-data1'), width=6),
-            dbc.Col(html.Div(pricetbl('tbl2', getprice()[0]), id='stock-data2'), width=6),
+            dbc.Col([
+                  html.H2('Symbols - Above Open', style = {'color': 'rgb(200, 200, 200)', 'font-family' : 'Verdana', 'text-align':'center'})
+                , html.Div(pricetbl('tbl1', getprice()[0]), id='stock-data1')
+            ], width=6),
+            dbc.Col([
+                  html.H2('Symbols - Below Open', style = {'color': 'rgb(200, 200, 200)', 'font-family' : 'Verdana', 'text-align':'center'})
+                , html.Div(pricetbl('tbl2', getprice()[0]), id='stock-data2'
+            )], width=6),
+          ])
+        , dbc.Row([
+            dbc.Col([
+                  html.H1('Banks', style = {'color': 'rgb(200, 200, 200)', 'font-family' : 'Verdana', 'text-align':'center'})
+                , html.Div(pricetbl('tbl5', getprice()[3]), id='stock-data5')
+            ], width=6),
+            dbc.Col([
+                  html.H1('IT', style = {'color': 'rgb(200, 200, 200)', 'font-family' : 'Verdana', 'text-align':'center'})
+                , html.Div(pricetbl('tbl6', getprice()[4]), id='stock-data6')
+            ], width=6),
+          ])
+        , dbc.Row([
+            dbc.Col([
+                  html.H1('Auto', style = {'color': 'rgb(200, 200, 200)', 'font-family' : 'Verdana', 'text-align':'center'})
+                , html.Div(pricetbl('tbl7', getprice()[5]), id='stock-data7')
+            ], width=6),
+            dbc.Col([
+                  html.H1('Pharma', style = {'color': 'rgb(200, 200, 200)', 'font-family' : 'Verdana', 'text-align':'center'})
+                , html.Div(pricetbl('tbl8', getprice()[6]), id='stock-data8')
+            ], width=6),
           ])
         , dcc.Interval(
                     id = 'interval-component',
-                    interval = 5 * 1000, # in milliseconds ie, 1 x 1000 means 1 sec
+                    interval = 1 * 1000, # in milliseconds ie, 1 x 1000 means 1 sec
                     n_intervals = 0
         )
     ], fluid = True)
@@ -239,12 +273,16 @@ app.layout = layout
     , Output('stock-data2', 'children')
     , Output('stock-data3', 'children')
     , Output('stock-data4', 'children')
+    , Output('stock-data5', 'children')
+    , Output('stock-data6', 'children')
+    , Output('stock-data7', 'children')
+    , Output('stock-data8', 'children')
 ], [Input('interval-component', 'n_intervals')])
 def update_table(x):
-    df, df_indices1, df_indices2 = getprice()
+    df, df_indices1, df_indices2, df_banks, df_it, df_auto, df_pharma, df_metal = getprice()
     dfu = df[df.changepct >=0].sort_values(by='changepct', ascending=False)
     dfd = df[df.changepct < 0].sort_values(by='changepct', ascending=True)
-    return pricetbl('tbl1', dfu), pricetbl('tbl2', dfd), indicestbl('tbl3', df_indices1), indicestbl('tbl4', df_indices2)
+    return pricetbl('tbl1', dfu), pricetbl('tbl2', dfd), indicestbl('tbl3', df_indices1), indicestbl('tbl4', df_indices2), pricetbl('tbl5', df_banks), pricetbl('tbl6', df_it), pricetbl('tbl7', df_auto), pricetbl('tbl8', df_pharma)
 
 
 if __name__ == '__main__':
